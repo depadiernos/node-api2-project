1119e5cbcce2318309a91919ede761ed24183983
Dennis Padiernos <depadiernos@gmail.com>
2019-12-06 21:10:03 -0600

    complete comment router methods.

--- routes/comments.js
+++ routes/comments.js
@@ -4,16 +4,48 @@ let db = require("../data/db");
 
 const router = express.Router({ mergeParams:true })
 
-router.post("/", ()=>{
-
+router.post("/", async (req, res)=>{
+    if (!req.params.id){
+        return res.status(404).json({message: "The post with the specified ID does not exist."})
+    }
+    if (!req.body.text) {
+        return res.status(400).json({error: "Please provide text for the comment."})
+    }
+    try {
+        const computedComment = { text: req.body.text }
+        const commentId = await db.insertComment(computedComment)
+        const newComment = await db.findCommentById(commentId)
+        res.status(201).json(newComment)
+    }
+    catch (err){
+        res.status(500).json({error: "There was an error while saving the comment to the database."})
+    }
 })
 
-router.get("/", ()=>{
-
+router.get("/", async (req, res)=>{
+    if (!req.params.id){
+        return res.status(404).json({message: "The post with the specified ID does not exist."})
+    }
+    try {
+        const comments = await db.findPostComments(req.params.id)
+        res.status(200).json(comments)
+    }
+    catch (err){
+        res.status(500).json({error: "The comment information could not be retrieved."})
+    }
 })
 
-router.get("/:commentId", ()=>{
-
+router.get("/:commentId", async (req, res)=>{
+    if (!req.params.id || !req.params.commentId){
+        return res.status(404).json({message: "The post or comment with the specified ID does not exist."})
+    }
+    try {
+        const comment = await db.findCommentById(req.params.commentId)
+        res.status(200).json(comment)
+    }
+    catch(err){
+        res.status(500).json({error: "The comment with the specified ID could not be retrieved."})
+    }
 })
 
 module.exports = router

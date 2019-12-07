012ce061261ec0e35fe568fc22172714180cfb88
Dennis Padiernos <depadiernos@gmail.com>
2019-12-06 20:06:48 -0600

    scaffolded comments router

---
+++ routes/comments.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const cors = require("cors");
+let db = require("../data/db");
+
+const router = express.Router({ mergeParams:true })
+
+router.post("/", ()=>{
+
+})
+
+router.get("/", ()=>{
+
+})
+
+router.get("/:commentId", ()=>{
+
+})
+
+module.exports = router
